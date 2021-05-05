857da6e98f1f0c0c7852ed5b2639bbae5531b4d9
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-05-04 22:37:42 -0400

    uwuwu ya casii

---
+++ js/formulario.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario);
+});
+
+function validarFormulario(evento) {
+    evento.preventDefault();
+    var nombre = document.getElementById('nombre').value;
+    if (nombre.length == 0) {
+        alert('Tiene que escribir su nombre');
+        document.formulario.nombre.focus();
+        return;
+    }
+    var correo = document.getElementById('correo').value;
+    if (correo.length < 6) {
+        alert('El correo no es válido');
+        return;
+    }
+    var telefono = document.getElementById('telefono').value;
+    if (telefono.length != 9) {
+        alert('El telefono no es válido');
+        return;
+    }
+    var mensaje = document.getElementById('mensaje')
+    if (mensaje.length < 25) {
+        alert('El largo mínimo de su mensaje es de 25 caracteres')
+    }
+    this.submit();
+}

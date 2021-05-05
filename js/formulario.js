4011eb97938f01a80cf3c75f5743d582306d165e
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-05-05 12:12:25 -0400

    validacionesss con ajax 8)8)

--- js/formulario.js
+++ js/formulario.js
@@ -1,28 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario);
+$(document).ready(function() {
+    $("#formulario").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            },
+            telefono: {
+                required: true,
+                number: true,
+                minlength: 9
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            textarea: {
+                required: true,
+                minlength: 25
+            }
+        },
+        messages: {
+            name: {
+                required: "Por favor escribe tu nombre",
+                minlength: "El nombre debe tener al menos 3 caracteres"
+            },
+            telefono: {
+                required: "Por favor ingrese un número de teléfono",
+                number: "Por favor ingresa solo números.",
+                minlength: "El largo de tu teléfono deben ser 9 caracteres"
+            },
+            email: {
+                required: "Por favor escribe tu correo",
+                email: "El email debe estar en el formato: abc@domain.com"
+            },
+            textarea: {
+                required: "Por favor escribe tu mensaje",
+                minlength: "Tu mensaje debe ser de al menos 25 caracteres"
+            }
+        }
+    });
 });
-
-function validarFormulario(evento) {
-    evento.preventDefault();
-    var nombre = document.getElementById('nombre').value;
-    if (nombre.length == 0) {
-        alert('Tiene que escribir su nombre');
-        document.formulario.nombre.focus();
-        return;
-    }
-    var correo = document.getElementById('correo').value;
-    if (correo.length < 6) {
-        alert('El correo no es válido');
-        return;
-    }
-    var telefono = document.getElementById('telefono').value;
-    if (telefono.length != 9) {
-        alert('El telefono no es válido');
-        return;
-    }
-    var mensaje = document.getElementById('mensaje').value;
-    if (mensaje.length < 25) {
-        alert('El largo mínimo de su mensaje es de 25 caracteres')
-    }
-    this.submit();
-}

86d8ebe61cc42e6be846fe8e6f0c715d1248d5ba
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-05-04 20:40:02 -0400

    link de ruta y funcionalidad boton cerrar

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,12 @@ let img = document.querySelector('#modal__img');
 let boton = document.querySelector('#modal__boton');
 
 for (let i = 0; i < imagenes.length; i++) {
-    imagenes[i].addEventListener('click', function() {
+    imagenes[i].addEventListener('click', function(e) {
         modal.classList.toggle("modal--open");
-    })
+        img.setAttribute("src", e.target.src);
+    });
 }
+
+boton.addEventListener('click', function() {
+    modal.classList.toggle("modal--open")
+})

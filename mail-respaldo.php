3c7ace7522b858176e66f9c502bd9a27910fe386
PhantomDuoc <hu.cerdaj@alumnos.duoc.cl>
2021-05-06 00:02:54 -0400

    bbbb

--- mail-respaldo.php
+++ mail-respaldo.php
@@ -1,13 +1,20 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+$nombre = $_POST["name"];
+$email = $_POST["email"];
+$telefono = $_POST["telefono"];
+$textarea = $_POST["textarea"];
+
+$body = "Nombre: " . $nombre
+. "<br>Correo: " . $email
+. "<br>Telefono: " . $telefono
+. "<br> Mensaje: " . $textarea;
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\STMP;
 require 'PHPMailer\PHPMailer.php';
 require 'PHPMailer\SMTP.php';
 require 'PHPMailer\Exception.php';
-
 
 
 //Instantiation and passing `true` enables exceptions
@@ -15,32 +22,22 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'veterinariacola@gmail.com';                     //SMTP username
     $mail->Password   = 'potito123H';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('veterinariacola@gmail.com', 'Veterinaria Cola');
     $mail->addAddress('veterinariacola@gmail.com');     //Add a recipient
 
-    $nombre = $_POST["name"];
-    $email = $_POST["email"];
-    $telefono = $_POST["telefono"];
-    $textarea = $_POST["textarea"];
-    
-    $body = "Nombre: " . $nombre
-    . "<br>Correo: " . $email
-    . "<br>Telefono: " . $telefono
-    . "<br> Mensaje: " . $textarea;
-
-    //Content
+   //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $nombre;
+    $mail->Subject = 'Asunto';
     $mail->Body    = $body;
     $mail->send();
     echo 'El mensaje se envió correctamente
